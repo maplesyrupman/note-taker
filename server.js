90735dace1159748bc2c686d6f044c26eec23f16
maplesyrupman <maplesyrupman@protonmail.com>
2021-11-07 12:21:26 -0500

    * route serves index.html, README.md

--- server.js
+++ server.js
@@ -43,6 +43,10 @@ app.delete('/api/notes/:id', (req, res) => {
     res.status(200).json({ message: 'success' })
 })
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`)
